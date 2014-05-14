7daadf6975e45e5cbf159475a8c4b542302a5f3d
bartavelle <bartavelle@gmail.com>
2014-05-14 22:42:40 +0200

    First prototype of the console game

---
+++ Backends/Common.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- | Some common functions, that work with most text based backends
+module Backends.Common where
+
+import Startups.Base
+import Startups.Cards
+import Startups.GameTypes
+import Startups.Game
+import Startups.PrettyPrint
+import Startups.Utils
+import Backends.Pure
+
+import qualified Data.Map.Strict as M
+import Control.Lens
+import Data.Monoid
+import qualified Data.Foldable as F
+import System.Random (mkStdGen)
+import Data.List (sort)
+
+cardpreview :: PlayerId -> M.Map PlayerId PlayerState -> Card -> PrettyDoc
+cardpreview pid pmap card =
+    let (f, vp) = cardEffectPreview pid card pmap
+        pvp = victory vp CompanyVictory
+        pf = pe f
+    in  case (f, vp) of
+            (0, 0) -> mempty
+            (0, _) -> brackets pvp
+            (_, 0) -> brackets pf
+            _      -> brackets (pf <> "/" <> pvp)
+
+playerActionsDialog :: PlayerId -> M.Map PlayerId PlayerState -> [Card] -> [(PlayerAction, Exchange, Maybe SpecialInformation)] -> PrettyDoc
+playerActionsDialog pid pmap clist actions =
+        "Your hand:"
+        </> vcat [ shortCard card <+> cardpreview pid pmap card | card <- clist ]
+        </> "What are you going to play ?"
+        </> vcat [ numerical (n :: Int) <> ") " <> desc pa | (n,pa) <- zip [0..] actions ]
+    where
+        desc (PlayerAction a card, exch, si) = actiondesc a <+> cardName card
+                                                            <+> exchdesc exch
+                                                            <> sidesc si
+        actiondesc Play = withCardColor Infrastructure "Play"
+        actiondesc Drop = withCardColor HeadHunting    "Drop"
+        actiondesc BuildCompany = withCardColor Community "Build a company stage using"
+        nn n = pmap ^. ix pid . neighbor n
+        exchdesc = sepBy ", " . map (\(n,v) -> "Exchanged" <+> F.foldMap pe v <+> "with" <+> showPlayerId (nn n)) . itoList
+        sidesc (Just UseOpportunity) = " using the opportunity capability"
+        sidesc Nothing = mempty
+
+cardChoiceDialog :: PlayerId -> M.Map PlayerId PlayerState -> [Card] -> PrettyDoc
+cardChoiceDialog pid pmap cards = vcat [ numerical (n :: Int) <> ") " <> desc c | (n,c) <- zip [0..] cards ]
+    where
+        desc c = shortCard c <+> cardpreview pid pmap c
+
+playerDescShort :: PlayerState -> PrettyDoc
+playerDescShort p@(PlayerState c cs _ f _ _) = brackets (pe c <> "-" <> pe cs)
+                                                <+> brackets (pe NLeft <> nn NLeft <+> nn NRight <> pe NRight)
+                                                <+> pe f
+                                                <+> availableresources
+                                                <+> poach
+                                                <+> science
+    where
+        nn ne = pe (view (neighbor ne) p)
+        availableresources = simpleres <+> multires
+        simpleres = F.foldMap pe $ sort $ concatMap (\(r,n,_) -> replicate n r) $ p ^.. cardEffects . _ProvideResource
+        multires = F.foldMap (brackets . F.foldMap pe . F.toList) $ p ^.. cardEffects . _ResourceChoice . _1
+        poach = pe $ p ^. cardEffects . _Poaching
+        science = F.foldMap pe $ sort $ p ^.. cardEffects . _RnD
+
+quicksituation :: Age -> Turn -> M.Map PlayerId PlayerState -> PrettyDoc
+quicksituation age turn stt = vcat $ hdr : map (\(n,ps) -> showPlayerId n <+> brackets (victory (vicmap ^. ix n) CompanyVictory <> "♚") <+> playerDescShort ps) (M.toList stt)
+    where
+        victorydetails = runPure (mkStdGen 0) (GameState stt [] (mkStdGen 0)) victoryPoints ^. _2 . _Right
+        vicmap :: M.Map PlayerId VictoryPoint
+        vicmap = victorydetails & traverse %~ (mconcat . M.elems)
+        hdr = "Age" <+> pe age <+> ", turn" <+> numerical turn
+
---
+++ src/Console.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Startups.Interpreter
+import Startups.Utils
+import Startups.Game
+import Startups.GameTypes
+import Backends.Common
+
+import Control.Lens
+import Control.Applicative
+import System.Random
+import qualified Text.PrettyPrint.ANSI.Leijen as PP
+
+readNumber :: IO Int
+readNumber = do
+    n <- getLine
+    return $ if all (\x -> x >= '0' && x <= '9') n
+        then read n
+        else -1
+
+consoleDict :: OperationDict IO
+consoleDict = OperationDict pd ac tp gm
+    where
+        pd age turn pid necards stt = do
+            let cards = _NonEmpty # necards
+                pm = stt ^. playermap
+                x = allowableActions age pid cards pm
+            r <- case pid of
+                    "you" -> do
+                        print (PP.pretty (quicksituation age turn pm))
+                        print (PP.pretty (playerActionsDialog pid pm cards x))
+                        readNumber
+                    _ -> randomRIO (0, length x - 1)
+            if r >= 0 && r < length x
+                then let (pa,e,_) = x !! r
+                     in  return (pa, e)
+                else pd age turn pid necards stt
+        ac turn pid necards stt msg = do
+            let cards = _NonEmpty # necards
+                pm = stt ^. playermap
+            case pid of
+                "you" -> do
+                    print (PP.pretty (cardChoiceDialog pid pm cards))
+                    n <- readNumber
+                    if n >= 0 && n < length cards
+                        then return (cards !! n)
+                        else ac turn pid necards stt msg
+                _ -> (cards !!) <$> randomRIO (0, length cards - 1)
+        tp "you" msg = print (PP.pretty msg)
+        tp _ _ = return ()
+        gm msg = print (PP.pretty msg)
+
+main :: IO ()
+main = do
+    g <- newStdGen
+    (_, o) <- runInterpreter consoleDict (initialGameState g ["you","pim","pam"]) playGame
+    case o of
+        Left rr -> error (show (PP.pretty rr))
+        Right x -> print x
